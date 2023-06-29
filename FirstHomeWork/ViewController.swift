212ec01e1ade248cd35a1ad840ba7e026436b979
Sakuwoo <kahorovasivara@gmail.com>
2023-06-29 11:54:12 +0600

    Initial Commit

---
+++ FirstHomeWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstHomeWork
+//
+//  Created by Sevara on 29/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
